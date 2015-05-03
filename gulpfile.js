4de32d6ead66279fc53705e291794c10bd5bc2ea
Samuel Hilson <samrhilson@gmail.com>
2015-05-02 23:55:46 -0500

    lets try this

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('push', ['commit'], function(){
       question: 'Do you want to push? :',
       input: '_key:y'
     }))
-    .pipe(exec('git push origin master', logOutput));
+    exec('git push origin master', logOutput);
 });
 
 gulp.task('publish', ['commit', 'push']);
