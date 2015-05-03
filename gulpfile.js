703f22ea82ed44582b4d5dc191dd89b3aeed0f6b
Samuel Hilson <samrhilson@gmail.com>
2015-05-02 20:36:07 -0500

    maybe fixed gulp proces

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-var gulp = require('./node_modules/gulp');
+var gulp = require('gulp');
 var config = require('./package.json');
-var concat = require('./node_modules/gulp-concat');
+var concat = require('gulp-concat');
 var exec = require('child_process').exec;
-var header = require('./node_modules/gulp-header');
-var footer = require('./node_modules/gulp-footer');
-var git = require('./node_modules/gulp-git');
-var prompt = require('./node_modules/gulp-prompt');
+var header = require('gulp-header');
+var footer = require('gulp-footer');
+var git = require('gulp-git');
+var prompt = require('gulp-prompt');
+var runSequence = require('run-sequence');
+
 
 var apibFiles = [
     './**/*.apib',
@@ -40,26 +42,26 @@ gulp.task('add', function(){
 // git commit task with gulp prompt
 gulp.task('commit', function(){
     // just source anything here - we just wan't to call the prompt for now
-    gulp.src('package.json')
+    return gulp.src('package.json')
     .pipe(prompt.prompt({
         type: 'input',
         name: 'commit',
         message: 'Please enter commit message...'
     },  function(res){
-      // now add all files that should be committed
-      // but make sure to exclude the .gitignored ones, since gulp-git tries to commit them, too
-      return gulp.src([ '!node_modules/', './*' ], {buffer:false})
+      return gulp.src([ '!node_modules/', './*.apib' ], {buffer:false})
       .pipe(git.commit(res.commit));
     }));
 });
 
-gulp.task('push', function(){
+gulp.task('push', ['commit'], function(){
   git.push('origin', 'master', function (err) {
     if (err) throw err;
   });
 });
 
-gulp.task('publish', ['push']);
+gulp.task('publish', function() {
+    runSequence('build','commit', 'push');
+});
 
 gulp.task('connect', function() {
     exec('apiary preview --server --port=' + config.apiaryPreviewPort, logOutput);
